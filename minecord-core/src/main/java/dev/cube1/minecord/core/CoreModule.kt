def2d17135f3e7956624b55747f9082a6278d9ad
projecttl <me@projecttl.net>
2022-06-15 19:56:31 +0900

    feat: middle save #2

--- minecord-core/src/main/java/dev/cube1/minecord/core/CoreModule.kt
+++ minecord-core/src/main/java/dev/cube1/minecord/core/CoreModule.kt
@@ -20,8 +20,13 @@ var inviteUrl: String
     get() = instance.config.getString("invite_url").toString()
     set(value) = instance.config.set("invite_url", value)
 
-val showActivity = instance.config.getBoolean("show_activity")
-val serverAddress = instance.config.getString("server_address").toString()
+var showActivity: Boolean
+    get() = instance.config.getBoolean("show_activity")
+    set(value) = instance.config.set("show_activity", value)
+
+var serverAddress: String
+    get() = instance.config.getString("server_address").toString()
+    set(value) = instance.config.set("server_address", value)
 
 fun jdaInit() {
     val builder = JDABuilder.createDefault(token).apply {
--- minecord-plugin/src/main/kotlin/dev/cube1/minecord/CommandDispatcher.kt
+++ minecord-plugin/src/main/kotlin/dev/cube1/minecord/CommandDispatcher.kt
@@ -1,6 +1,8 @@
 package dev.cube1.minecord
 
 import dev.cube1.minecord.core.inviteUrl
+import dev.cube1.minecord.core.serverAddress
+import dev.cube1.minecord.core.showActivity
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
 import org.bukkit.command.Command
@@ -11,15 +13,13 @@ import org.bukkit.command.TabCompleter
 object CommandDispatcher : CommandExecutor, TabCompleter {
 
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
-        if (command.name != "invite") return false
-
-        when (args.size) {
-            0 -> {
-                sender.sendMessage("디스코드 주소: $inviteUrl")
+        return when (command.name) {
+            "invite" -> {
+                if (args.isEmpty()) {
+                    sender.sendMessage("디스코드 주소: $inviteUrl")
                     return true
                 }
 
-            2 -> {
                 if (args[0] == "set" && sender.isOp) {
                     val url = try {
                         args[1]
@@ -29,15 +29,98 @@ object CommandDispatcher : CommandExecutor, TabCompleter {
                     }
 
                     inviteUrl = url
-                    sender.sendMessage(Component.text("디스코드 주소가 설정 되었습니다: ", NamedTextColor.GREEN)
-                        .append(Component.text(url, NamedTextColor.WHITE)))
+                    sender.sendMessage(Component.text(
+                        "디스코드 주소가 설정 되었습니다: ",
+                        NamedTextColor.GREEN
+                    ).append(Component.text(
+                        url,
+                        NamedTextColor.WHITE
+                    )))
 
                     return true
                 }
-            }
-        }
 
-        return false
+                false
+            }
+
+            "minecord" -> {
+                if (args.isEmpty()) {
+                    sender.sendMessage(Component.text("매개 변수를 입력해 주세요!", NamedTextColor.RED))
+                    return false
+                }
+
+                if (!sender.isOp) {
+                    sender.sendMessage(Component.text("이 명령어는 관리자만 사용할 수 있습니다!", NamedTextColor.RED))
+                    return false
+                }
+
+                when (args[0]) {
+                    "embed" -> {
+                        when (args[1]) {
+                            else -> return false
+                        }
+
+                        return false
+                    }
+
+                    "activity" -> {
+                        when (args[1]) {
+                            "set" -> {
+                                val addr = try {
+                                    args[2]
+                                } catch (exception: ArrayIndexOutOfBoundsException) {
+                                    sender.sendMessage(Component.text("주소를 입력해 주세요!", NamedTextColor.RED))
+                                    return false
+                                }
+
+                                serverAddress = addr
+                                return true
+                            }
+
+                            "toggle" -> {
+                                showActivity = when (args[2]) {
+                                    "on" -> {
+                                        sender.sendMessage(Component.text()
+                                            .content("Activity toggle 설정을").color(NamedTextColor.GREEN)
+                                            .content("'on'").color(NamedTextColor.YELLOW)
+                                            .content("으로 설정 하셨습니다.").color(NamedTextColor.GREEN)
+                                        )
+                                        true
+                                    }
+
+                                    "off" -> {
+                                        sender.sendMessage(Component.text()
+                                            .content("Activity toggle 설정을").color(NamedTextColor.GREEN)
+                                            .content("'on'").color(NamedTextColor.YELLOW)
+                                            .content("으로 설정 하셨습니다.").color(NamedTextColor.GREEN)
+                                        )
+                                        false
+                                    }
+                                    else -> {
+                                        sender.sendMessage(Component.text()
+                                            .content("'on'").color(NamedTextColor.RED)
+                                            .content("'또는'").color(NamedTextColor.RED)
+                                            .content("'off'").color(NamedTextColor.YELLOW)
+                                            .content("만 입력해 주시기 바랍니다.").color(NamedTextColor.RED)
+                                        )
+
+                                        return false
+                                    }
+                                }
+
+                                return true
+                            }
+
+                            else -> return false
+                        }
+                    }
+                }
+
+                false
+            }
+
+            else -> false
+        }
     }
 
     override fun onTabComplete(sender: CommandSender, command: Command, alias: String, args: Array<out String>): MutableList<String>? {
